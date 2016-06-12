0b329c79bee2586696f60765d882370959f110b5
logiclogue <jordanpaullord@hotmail.co.uk>
2016-06-12 18:22:25 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var sim = require('football-score-sim');
+var Match = sim.Match;
+var Team = sim.Team;
+
+var groupA = new Group([france, romania, albania, switzerland]);
+var groupB = new Group([wales, slovakia, england, russia]);
+var groupC = new Group([poland, northernIreland, germany, ukraine]);
+var groupD = new Group([turkey, croatia, spain, czechRepublic]);
+var groupE = new Group([republicOfIreland, sweden, belgium, italy]);
+var groupF = new Group([austria, hungary, portugal, iceland]);
+
+
+console.log(Match);
---
+++ js/League.js
@@ -0,0 +1,17 @@
+var sim = require('football-score-sim');
+
+
+/*
+ * Creates a league. Can simulate all the games.
+ */
+function League(teams) {
+    this.teams = teams || [];
+}
+
+(function (static_, proto_) {
+
+
+
+}(League, League.prototype));
+
+module.exports = League;
