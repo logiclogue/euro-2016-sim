8b7c86ae3264ce9f47226c6405a583ff91d2431a
logiclogue <jordanpaullord@hotmail.co.uk>
2016-06-12 20:41:37 +0100

    Adds getTeam method

---
+++ js/getTeam.js
@@ -0,0 +1,11 @@
+var Team = require('football-score-sim').Team;
+var teams = require('../res/teams.json');
+
+
+/*
+ * Method which is used to retreive a team from
+ * the json file and create a team object.
+ */
+module.exports = function (teamName) {
+    var data = teams[teamName];
+}
