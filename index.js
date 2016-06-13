98a9db9739291e1d35f758f82e3c932704acaa07
logiclogue <jordanpaullord@hotmail.co.uk>
2016-06-13 09:36:58 +0100

    Adds sort method to League

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var getTeam = require('./js/getTeam');
 
 
 var groupA = new League([getTeam('France'), getTeam('Romania'), getTeam('Albania'), getTeam('Switzerland')]);
-var groupb = new League([getTeam('wales'), getTeam('Slovakia'), getTeam('England'), getTeam('Russia')]);
+var groupB = new League([getTeam('Wales'), getTeam('Slovakia'), getTeam('England'), getTeam('Russia')]);
 var groupC = new League([getTeam('Poland'), getTeam('Northern Ireland'), getTeam('Germany'), getTeam('Ukraine')]);
 var groupD = new League([getTeam('Turkey'), getTeam('Croatia'), getTeam('Spain'), getTeam('Czech Republic')]);
 var groupE = new League([getTeam('Republic Of Ireland'), getTeam('Sweden'), getTeam('Belgium'), getTeam('Italy')]);
 var groupF = new League([getTeam('Austria'), getTeam('Hungary'), getTeam('Portugal'), getTeam('Iceland')]);
 
-groupA.simulate();
+groupB.simulate();
+groupB.sort();
+console.log(groupB.teams);
--- js/League.js
+++ js/League.js
@@ -8,6 +8,16 @@ function League(teams) {
     this.teams = teams || [];
     this.matches = [];
 
+    this.teams.forEach(function (team) {
+        team.points = 0;
+        team.played = 0;
+        team.wins = 0;
+        team.draws = 0;
+        team.loses = 0;
+        team.goalsFor = 0;
+        team.goalsAgainst = 0;
+    });
+
     this._generateFixtureList();
 }
 
@@ -20,9 +30,54 @@ function League(teams) {
     proto_.simulate = function () {
         this.matches.forEach(function (match) {
             var result = match.result();
+            var teamA = this.teams[this.teams.indexOf(match.team[0])];
+            var teamB = this.teams[this.teams.indexOf(match.team[1])];
 
-            console.log(result);
+            teamA.played += 1;
+            teamA.goalsFor += result.score[0];
+            teamA.goalsAgainst += result.score[1];
+
+            teamB.played += 1;
+            teamB.goalsFor += result.score[1];
+            teamB.goalsAgainst += result.score[0];
+
+            // If teamA won.
+            if (result.result === 0) {
+                teamA.points += 3;
+                teamA.wins += 1;
+                
+                teamB.loses += 1;
+            }
+            // If teamB won.
+            else if (result.result === 1) {
+                teamB.points += 3;
+                teamB.wins += 1;
+
+                teamA.loses += 1;
+            }
+            // If the game was a draw.
+            else {
+                teamA.points += 1;
+                teamA.draws += 1;
+
+                teamB.points += 1;
+                teamB.draws += 1;
+            }
         }.bind(this));
+    };
+
+    /*
+     * Sorts the teams array based on number of
+     * points.
+     */
+    proto_.sort = function () {
+        this.teams.sort(function (teamA, teamB) {
+            if (teamA.points === teamB.points) {
+                return teamA.goalsFor - teamA.goalsAgainst < teamB.goalsFor - teamB.goalsAgainst;
+            }
+
+            return teamA.points < teamB.points;
+        });
     };
 
 
