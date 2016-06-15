3fe47e59a3bad18fad4841429ccfc621b8a932ba
logiclogue <jordanpaullord@hotmail.co.uk>
2016-06-15 10:45:44 +0100

    Adds methods to KnockOut class

--- js/KnockOut.js
+++ js/KnockOut.js
@@ -1,10 +1,42 @@
 function KnockOut(teams) {
-    
+    this.rounds = [];
+    this.matches = [];
+    this.teams = teams;
+
+    this._generateMatches();
 }
 
 (function (static_, proto_) {
 
+    /*
+     * Simulates the whole round.
+     */
+    proto_.simulateRound = function () {
+        
+    };
 
+    /*
+     * Simulates the next match.
+     */
+    proto_.simulateMatch = function () {
+        
+    };
+
+
+    /*
+     * Checks to see if all the games in the round
+     * have been complete.
+     */
+    proto_._checkRoundComplete = function () {
+        
+    };
+
+    /*
+     * Generate matches.
+     */
+    proto_._generateMatches = function () {
+
+    };
 
 }(KnockOut, KnockOut.prototype));
 
